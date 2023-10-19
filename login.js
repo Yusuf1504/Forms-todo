6c5db866159304de427740bb4b66a36afe211474
Yusuf1504 <mohammedyusuf021@gmail.com>
2023-10-19 22:17:26 +0530

    improvised

--- login.js
+++ login.js
@@ -4,7 +4,7 @@ var email_error = document.getElementById("email_error");
 var pass_error = document.getElementById("pass_error");
 
 email.addEventListener('textInput', email_Verify);
-password.addEventListener('textInput', email_Verify);
+password.addEventListener('textInput', pass_Verify);
 
 function validated() {
     if (email.value.length < 9) {
@@ -13,7 +13,7 @@ function validated() {
         email.focus();
         return false;
     }
-    if (password.value.length < 9) {
+    if (password.value.length < 6) {
         password.style.border = "1px solid red";
         pass_error.style.display ="block";
         password.focus();
@@ -27,7 +27,7 @@ function email_Verify() {
         return true;
     }
     function pass_Verify() {
-        if (password.value.length >= 8) {
+        if (password.value.length >= 5) {
             password.style.border = "1px solid silver";
             pass_error.style.display = "none"
             return true;
